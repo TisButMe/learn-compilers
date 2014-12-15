269e9d3b339f348aedc4bc2400155fb5e7d1108e
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-12-15 16:59:47 +0100

    Switched the call type of emit_ln() to &str reduce the nb of to_string() everywhere

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@ use std::io::stdin;
 
 fn main() {
   let mut look_ahead = ' ';
-  emit_ln("section .text".to_string());
-  emit_ln("global _start".to_string());
-  emit_ln("_start:".to_string());
-  emit_ln(";; start of code gen".to_string());
+  emit_ln("section .text");
+  emit_ln("global _start");
+  emit_ln("_start:");
+  emit_ln(";; start of code gen");
   expression(&mut look_ahead);
-  if look_ahead != '\n' {panic!(expected("Newline as the end of the input".to_string()));}
-  emit_ln("\n\t;; end of code gen\n\tMOV RAX, 60".to_string());
-  emit_ln("XOR RDI, RDI".to_string());
-  emit_ln("SYSCALL".to_string());
+  if look_ahead != '\n' {panic!(expected("Newline as the end of the input"));}
+  emit_ln("\n\t;; end of code gen\n\tMOV RAX, 60");
+  emit_ln("XOR RDI, RDI");
+  emit_ln("SYSCALL");
 }
 
 fn expression(look_ahead: &mut char) {
   term(look_ahead);
   while vec!('+', '-').contains(look_ahead) {
-    emit_ln("PUSH RAX".to_string());
+    emit_ln("PUSH RAX");
     match *look_ahead {
       '+' => add(look_ahead),
       '-' => sub(look_ahead),
-      _   => panic!(expected("addop".to_string()))
+      _   => panic!(expected("addop"))
     }
   }
 }
@@ -28,40 +28,40 @@ fn expression(look_ahead: &mut char) {
 fn term(look_ahead: &mut char) {
   factor(look_ahead);
   while vec!('*', '/').contains(look_ahead) {
-    emit_ln("PUSH RAX".to_string());
+    emit_ln("PUSH RAX");
     match *look_ahead {
       '*' => mul(look_ahead),
       '/' => div(look_ahead),
-      _   => panic!(expected("mulop".to_string()))
+      _   => panic!(expected("mulop"))
     }
   }
 }
 
 fn add(look_ahead: &mut char) {
   term(look_ahead);
-  emit_ln("POP RCX".to_string());
-  emit_ln("ADD RAX, RCX".to_string());
+  emit_ln("POP RCX");
+  emit_ln("ADD RAX, RCX");
 }
 
 fn sub(look_ahead: &mut char) {
   term(look_ahead);
-  emit_ln("POP RCX".to_string());
-  emit_ln("SUB RAX, RCX".to_string());
-  emit_ln("NEG RAX".to_string());
+  emit_ln("POP RCX");
+  emit_ln("SUB RAX, RCX");
+  emit_ln("NEG RAX");
 }
 
 fn mul(look_ahead: &mut char) {
   factor(look_ahead);
-  emit_ln("POP RCX".to_string());
-  emit_ln("IMUL RCX".to_string());
+  emit_ln("POP RCX");
+  emit_ln("IMUL RCX");
 }
 
 fn div(look_ahead: &mut char) {
   factor(look_ahead);
-  emit_ln("MOV RCX, RAX".to_string());
-  emit_ln("POP RAX".to_string());
-  emit_ln("XOR RDX, RDX".to_string());
-  emit_ln("IDIV RCX".to_string());
+  emit_ln("MOV RCX, RAX");
+  emit_ln("POP RAX");
+  emit_ln("XOR RDX, RDX");
+  emit_ln("IDIV RCX");
 }
 
 fn factor(look_ahead: &mut char) {
@@ -72,15 +72,16 @@ fn factor(look_ahead: &mut char) {
       expression(look_ahead);
 
       if *look_ahead != ')' {
-        panic!(expected("delimiter: )".to_string()));
+        panic!(expected("delimiter: )"));
       }
 
       *look_ahead = next_char();
     },
-    '+'|'-'                => emit_ln("XOR RAX, RAX".to_string()),
+    '+'|'-'                => emit_ln("XOR RAX, RAX"),
     x if x.is_alphabetic() => ident(look_ahead),
     _                      => {
-      emit_ln("MOV RAX, ".to_string() + get_number(look_ahead).to_string());
+      let instr = "MOV RAX, ".to_string() + get_number(look_ahead).to_string();
+      emit_ln(instr.as_slice());
       *look_ahead = next_char();
     }
   }
@@ -93,7 +94,7 @@ fn next_char() -> char {
 fn get_number(look_ahead: &char) -> uint {
   match look_ahead.to_digit(10) {
     Some(n) => n,
-    None    => panic!(expected("Integer".to_string()))
+    None    => panic!(expected("Integer"))
   }
 }
 
@@ -106,11 +107,15 @@ fn ident(look_ahead: &mut char) {
   *look_ahead = next_char();
   match *look_ahead {
     '(' => {
-      if next_char() != ')' { panic!(expected("end of function parameters delimiter: )".to_string())); }
-      emit_ln("CALL ".to_string() + name);
+      if next_char() != ')' { panic!(expected("end of function parameters delimiter: )")); }
+      let instr = "CALL ".to_string() + name;
+      emit_ln(instr.as_slice());
       *look_ahead = next_char();
     },
-    _ => emit_ln("MOV RAX, ".to_string() + name)
+    _ => {
+      let instr = "MOV RAX, ".to_string() + name;
+      emit_ln(instr.as_slice());
+    }
   }
 }
 
@@ -118,14 +123,14 @@ fn error(s: String) -> String {
   format!("Error: {}.", s)
 }
 
-fn expected(s: String) -> String {
-  error(s + " expected".to_string())
+fn expected(s: &str) -> String {
+  error(s.to_string() + " expected")
 }
 
 fn emit(s: String) {
   print!("\t{}", s);
 }
 
-fn emit_ln(s: String) {
-  emit(s + "\n".to_string());
+fn emit_ln(s: &str) {
+  emit(s.to_string() + "\n".to_string());
 }
