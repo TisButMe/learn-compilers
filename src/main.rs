0fcf835f05fa7c1ab178b42959e8c8cb353e2956
Thomas Poinsot <tomitom007@gmail.com>
2014-12-21 13:45:11 +0100

    Interpeter now handles addop correctlyy

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 use std::io::stdin;
 
 fn main() {
-    let look_ahead: uint = ' ';
+    let mut look_ahead = next_char();
     expression(&mut look_ahead);
 }
 
 fn expression(look_ahead: &mut char) {
-    *look_ahead = 
+    let mut val = if is_addop(look_ahead) {
+        0
+    } else {
+        get_number(look_ahead)
+    };
+
+    *look_ahead = next_char();
+    while is_addop(look_ahead) {
+        match *look_ahead {
+            '+' => {
+                *look_ahead = next_char();
+                val += get_number(look_ahead);
+            }
+            '-' => {
+                *look_ahead = next_char();
+                val -= get_number(look_ahead);
+            }
+            _ => panic!(expected("addop"))
+        }
+        *look_ahead = next_char();
+    }
+    emit_ln(val.to_string().as_slice());
+}
+
+fn is_addop(c: &char) -> bool {
+    ['+', '-'].contains(c)
 }
 
 fn next_char() -> char {
     stdin().read_char().unwrap()
 }
 
-fn get_number() -> uint {
-    match next_char().to_digit(10) {
-        Some(n) => n,
+fn get_number(look_ahead: &char) -> int {
+    match look_ahead.to_digit(10) {
+        Some(n) => n.to_int().unwrap(),
         None    => panic!(expected("Integer"))
     }
 }
